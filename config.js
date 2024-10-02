aba99ead111dfebe9bcdad65d5fc223c6f7b5980
Liyo555 <gamingliyo82@gmail.com>
2024-10-02 10:52:31 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lq0zgJjD#c69y4Qj0R4IoMDUu76Otz973hT3nu6Tsbp_QEE-yjvg",
+SESSION_ID: process.env.SESSION_ID || "qeA2QL6T#3H4Ql5FgOtXbvSXyWNBcMk_-0MqFwV30lNJc7Ujf2ok",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 MONGODB: process.env.MONGODB || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
